0e453b848f32f7ce201a91a43c9649ef5547b44c
Caroline-Rajah <caroline9005@gmail.com>
2019-02-03 16:29:22 -0800

    Add files via upload

---
+++ friends.py
@@ -0,0 +1,59 @@
+users =[
+    { "id":0, "name": "Hero" },
+    { "id":1, "name": "Dunn" },
+    { "id":2, "name": "Sue" },
+    { "id":3, "name": "Chi" },
+    { "id":4, "name": "Thor" },
+    { "id":5, "name": "Clive" },
+    { "id":6, "name": "Hicks" },
+    { "id":7, "name": "Devin" },
+    { "id":8, "name": "Kate" },
+    { "id":9, "name": "Klein" }    
+]
+
+friendship = [
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (6, 8),
+    (7, 8),
+    (8, 9)
+]
+
+def num_friends(user):
+    '''
+    Find number of friends for a given user
+    '''
+    userId=None
+    
+    if user in users:
+        userId=user['id']
+        
+    count = 0
+    
+    if(userId!=None):
+        for tup in friendship:
+            if userId in tup:
+                count+=1
+            
+    return count
+
+def sort_by_num_friends():
+    '''README.md
+    Sort from "most friends" to "least friends"
+    '''
+    friend_count=[]
+    for user in users:
+        friend_count.append({'id':user['id'],'name':user['name'],'no_friends':num_friends(user)})
+    friend_count.sort(key=lambda k: k['no_friends'])
+    return friend_count
+
+sorted_list = sort_by_num_friends()
+for item in sorted_list:
+    print(f"{item['name']} has {item['no_friends']} friends")
